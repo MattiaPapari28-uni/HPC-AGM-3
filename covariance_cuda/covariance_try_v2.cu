36d5cd8535fb9e61bae23883909f85774036008d
gastanibar <tolveandrea943@gmail.com>
2024-12-07 17:16:09 +0100

    ciao

--- covariance_cuda/covariance_try_v2.cu
+++ covariance_cuda/covariance_try_v2.cu
@@ -12,16 +12,11 @@
 
 __device__ double atomicAddDouble(double* address, double val)
 {
-    unsigned long long int* address_as_ull =
-                              (unsigned long long int*)address;
+    unsigned long long int* address_as_ull = (unsigned long long int*)address;
     unsigned long long int old = *address_as_ull, assumed;
-
     do {
         assumed = old;
-        old = atomicCAS(address_as_ull, assumed,
-                        __double_as_longlong(val +
-                               __longlong_as_double(assumed)));
-
+        old = atomicCAS(address_as_ull, assumed,__double_as_longlong(val + __longlong_as_double(assumed)));
     // Note: uses integer comparison to avoid hang in case of NaN (since NaN != NaN)
     } while (assumed != old);
 
@@ -39,79 +34,34 @@ __global__ void kernel_calc_mean(DATA_TYPE* data, DATA_TYPE* mean)
     if (i < M and j < N) {
         data[i * M + j] = ((DATA_TYPE)i * j) / M;
         __syncthreads();
-        atomicAddDouble(&mean[j], data[i * M + j]);
-        __syncthreads();
 
+        atomicAddDouble(&mean[j], data[i * M + j]);
     }
 }
 
 __global__ void kernel_covariance(DATA_TYPE* data, DATA_TYPE* mean, int* flags, DATA_TYPE float_n, DATA_TYPE* symmat)
   {   
-    // Compute the row (i) and column (j) indices for this thread
-    int tidy = threadIdx.y; //indice della colonna
+    int tidy = threadIdx.y;
     int tidx = threadIdx.x;
-    int j = blockIdx.x * blockDim.x + tidx; //fino a M
+    int j = blockIdx.x * blockDim.x + tidx;
     int i = blockIdx.y * blockDim.y + tidy;
-    // Ensure we are within bounds
     if (i < M and j < N) {
-       if(atomicCAS(&flags[j],0,1) == 0) //si può testare rimuovendo il calcolo ed eseguendo direttamente sia sottrazione che divisione
+      if(atomicCAS(&flags[j],0,1) == 0) 
           mean[j] /= float_n;
-        __syncthreads();
+      __syncthreads();
        
-        atomicAddDouble(&data[i * M + j], -(mean[j]));
-        //__shared__ DATA_TYPE temp;
-
-        //int j1 = blockIdx.x * blockDim.x + threadIdx.x;
-        //int j2 = blockIdx.y * blockDim.y + threadIdx.y;
-        DATA_TYPE sum = 0.0;
-
-        // Ensure indices are within bounds
-		
-//	__syncthreads();
+      atomicAddDouble(&data[i * M + j], -(mean[j]));
+      DATA_TYPE sum = 0.0;
          
-	for (int k = 0; k < N; k++) {
-           //printf("%d\n", (data[k * M + j] * data[k * M + i]));
-	  // atomicAddDouble(&sum, (data[k * M + j] * data[k * M + i]));
-          sum += (data[k * M + j] * data[k * M + i]);
-	}
-
-	symmat[j * M + i] = sum;
-        symmat[i * M + j] = sum;
-	//symmat[i * M + j] = symmat[j * M + i]; 
-    }
-
-    //printf("%d\n", symmat[i]);
-
-  /*int i, j, j1, j2;
-
-  for (i = 0; i < M; i++)
-    for (j = 0; j < N; j++)
-      data[i j] = ((DATA_TYPE) i*j) / M;
-
-  
-    for (j = 0; j < M; j++)
-      {
-        mean[j] = 0.0;
-	for (i = 0; i < N; i++)
-	  mean[j] += data[i j];
-	mean[j] /= float_n;
-      }
-      
-    
-    for (i = 0; i < N; i++)
-      for (j = 0; j < M; j++)
-	      data[i j] -= mean[j];
-      
-    
-    /*for (j1 = 0; j1 < M; j1++)
-        for (j2 = j1; j2 < M; j2++)
-	      {
-          symmat[j1 j2] = 0.0;
-	        for (i   = 0; i < N; i++)
-	          symmat[j1][j2] += data[i][j1] * data[i][j2];
-	        symmat[j2][j1] = symmat[j1][j2];
+      if(j <= i){
+        for (int k = 0; k < N; k++) {
+            sum += (data[k * M + j] * data[k * M + i]);
         }
-    */
+
+        symmat[j * M + i] = sum;
+        symmat[i * M + j] = sum;  
+      }
+    }
 }
 
 static
@@ -137,29 +87,23 @@ int main(int argc, char** argv)
   /* Variable declaration/allocation. */
   DATA_TYPE float_n = 1.2;
   DATA_TYPE *h_symmat, *h_mean, *h_data;
-  DATA_TYPE *d_mean, *d_data, *d_symmat;
-  int* d_flags;
+  int* h_flags;
 
-  cudaMalloc(&d_flags, M * sizeof(int));
-  cudaMemset(d_flags, 0, M * sizeof(int)); //setto a 0 tutte le celle
-  cudaMallocHost((void**)&h_symmat,sizeof(DATA_TYPE) * M * M); 
-  cudaMallocHost((void**)&h_mean,sizeof(DATA_TYPE) * M); 
-  cudaMallocHost((void**)&h_data,sizeof(DATA_TYPE) * M * N); 
-  cudaMalloc((void**)&d_data, sizeof(DATA_TYPE) * M * N);
-  cudaMalloc((void**)&d_mean, sizeof(DATA_TYPE) * M);
-  cudaMalloc((void**)&d_symmat, sizeof(DATA_TYPE) * M * M);
-  cudaMemset(d_symmat, 0, M * M * sizeof(DATA_TYPE));
-  cudaMemset(d_mean, 0, M * sizeof(DATA_TYPE));
+  cudaMallocManaged((void**)&h_flags, M * sizeof(int));
+  cudaMallocManaged((void**)&h_symmat,sizeof(DATA_TYPE) * M * M); 
+  cudaMallocManaged((void**)&h_mean,sizeof(DATA_TYPE) * M); 
+  cudaMallocManaged((void**)&h_data,sizeof(DATA_TYPE) * M * N); 
+  cudaMemset(h_symmat, 0, M * M * sizeof(DATA_TYPE));
+  cudaMemset(h_mean, 0, M * sizeof(DATA_TYPE));
+  cudaMemset(h_flags, 0, M * sizeof(int)); //setto a 0 tutte le celle
   /* Start timer. */
-  clock_gettime(CLOCK_REALTIME, rt + 0); // non va dopo?
+  clock_gettime(CLOCK_REALTIME, rt + 0); 
   dim3 dimBlock(BLOCK_DIM, BLOCK_DIM); 
   dim3 dimGrid(((N+BLOCK_DIM-1)/BLOCK_DIM), ((N+BLOCK_DIM-1)/BLOCK_DIM));
   /* Run kernel. */
-  kernel_calc_mean<<<dimGrid,dimBlock>>>(d_data, d_mean); //calcolo il vettore mean in un kernel a parte così da poter risolvere i problemi di sincronizzazione  
-  cudaMemcpy(h_mean, d_mean, sizeof(DATA_TYPE) * M, cudaMemcpyDeviceToHost);
-  cudaMemcpy(h_data, d_data, sizeof(DATA_TYPE) * M * M, cudaMemcpyDeviceToHost);
-  kernel_covariance<<<dimGrid,dimBlock>>>(d_data, d_mean, d_flags, float_n, d_symmat);
-  cudaMemcpy(h_symmat, d_symmat, sizeof(DATA_TYPE) * M * M, cudaMemcpyDeviceToHost);
+  kernel_calc_mean<<<dimGrid,dimBlock>>>(h_data, h_mean); //calcolo il vettore mean in un kernel a parte così da poter risolvere i problemi di sincronizzazione  
+  kernel_covariance<<<dimGrid,dimBlock>>>(h_data, h_mean, h_flags, float_n, h_symmat);
+  cudaDeviceSynchronize();
   /* Stop and print timer. */
   clock_gettime(CLOCK_REALTIME, rt + 1);
   wt = (rt[1].tv_sec - rt[0].tv_sec) + 1.0e-9 * (rt[1].tv_nsec - rt[0].tv_nsec);
@@ -167,12 +111,10 @@ int main(int argc, char** argv)
   print_array(h_symmat);
 
   /* Be clean. */
-  cudaFree(d_data);
-  cudaFree(d_symmat);
-  cudaFree(d_mean);
-  cudaFreeHost(h_data);
-  cudaFreeHost(h_mean);
-  cudaFreeHost(h_symmat);
+  cudaFree(h_data);
+  cudaFree(h_symmat);
+  cudaFree(h_mean);
+  cudaFree(h_flags);
 
   return 0;
 }
